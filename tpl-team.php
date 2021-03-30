d83191b81562d4ddb4a40953d0015f6b1596577a
dmitriiii <dimitriy1988wl@gmail.com>
2021-03-30 10:29:49 +0300

    upd

--- tpl-team.php
+++ tpl-team.php
@@ -3,6 +3,15 @@
 Template Post Type: post, page */
 
 get_header();
+$pid = get_the_ID();
+$subtitle = get_field('subtitle',$pid);
+$title = get_field('title',$pid);
+$paragraph = get_field('paragraph',$pid);
+$teams = get_field('teams',$pid);
+
+// echo '<pre>';
+// print_r($teams);
+// echo '</pre>';
 
 
 if ( have_posts() ) : while ( have_posts() ) : the_post(); 
@@ -13,7 +22,63 @@ if ( have_posts() ) : while ( have_posts() ) : the_post();
 	?>
 
 
+	<div class="main__content">
+	<div class="team__info">
+		<div class="container">
+			<div class="team__group">
+				<?php the_content(); ?>
+			</div>
+		</div>
+	</div>
 
+	<div class="team__list">
+		<div class="container">
+			<div class="row">
+				<div class="heading">
+					<h4 class="subtitle subtitle__dot-before subtitle__dot-after"><?= $subtitle; ?></h4>
+					<h2 class="title"><?= $title; ?></h2>
+					<p class="paragraph"><?= $paragraph; ?></p>
+				</div>
+			</div>
+
+			<?php 
+			if(is_array($teams) && count($teams)>0): ?>
+			<div class="row">
+				<?php 
+				foreach ($teams as $value): ?>
+					<div class="col col-33">
+						<div class="team__card">
+							<?php 
+							$all_meta = get_user_meta( $value['user'] );
+ 	 						// echo '<pre>';
+ 	 						// print_r( $all_meta ); 
+ 	 						// echo '</pre>';
+							if($all_meta['user_photo'][0]):
+								echo M4Helpers::getImgHtml([ 'img_id'=>$all_meta['user_photo'][0], 'size'=>'medium']);
+							endif;?>
+							<!-- <img src="<?php bloginfo('template_directory');?>/assets/images/091224867853_huge.png" alt=""> -->
+
+							<div class="team__card_info">
+								<div class="team__name">
+									<?= $all_meta['first_name'][0] . ' ' . $all_meta['last_name'][0]; ?>
+								</div>
+								<div class="team__work"><?= $all_meta['description'][0]; ?></div>
+								<div class="team__social">
+									<a href="#" class="team__social_item team__social_add"></a>
+								</div>
+							</div>
+						</div>
+					</div>
+				<?php 
+				endforeach ?>
+			</div>
+			<?php 
+			endif; ?>
+		</div>
+	</div>
+
+
+</div>
 
 
 
@@ -21,148 +86,4 @@ if ( have_posts() ) : while ( have_posts() ) : the_post();
 <?php
 endwhile;
 endif;
-?>
-
-
-
-<div class="main__content">
-    <div class="team__info">
-        <div class="container">
-            <div class="team__group">
-
-                <div class="team__photo">
-                    <img src="<?php bloginfo('template_directory');?>/assets/images/2443423454.png" alt="">
-
-                    <div class="team__heading">
-                        <h4 class="subtitle subtitle__dot-before">Skilled team</h4>
-                        <h2 class="title">Meet Our Executive Team Members</h2>
-                        <p class="description">If we had a 'secret sauce' it would be our awesome people.</p>
-                    </div>
-                </div>
-
-                <section class="section partners">
-                    <div class="container container--fluid">
-                        <div class="row">
-                            <div class="col col-20">
-                                <div class="partners__item">
-                                    <img src="<?php bloginfo('template_directory');?>/assets/images/c-logo2-2.png" alt="SEO Mind">
-                                </div>
-                            </div>
-                            <div class="col col-20">
-                                <div class="partners__item">
-                                    <img src="<?php bloginfo('template_directory');?>/assets/images/c-logo2-1.png" alt="IBM Media">
-                                </div>
-                            </div>
-                            <div class="col col-20">
-                                <div class="partners__item">
-                                    <img src="<?php bloginfo('template_directory');?>/assets/images/c-logo2-6.png" alt="Greenhost">
-                                </div>
-                            </div>
-                            <div class="col col-20">
-                                <div class="partners__item">
-                                    <img src="<?php bloginfo('template_directory');?>/assets/images/c-logo2-5.png" alt="Yodgy">
-                                </div>
-                            </div>
-                            <div class="col col-20">
-                                <div class="partners__item">
-                                    <img src="<?php bloginfo('template_directory');?>/assets/images/c-logo2-4.png" alt="Atomic SEO">
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </section>
-            </div>
-        </div>
-    </div>
-
-    <div class="team__list">
-        <div class="container">
-            <div class="row">
-                <div class="heading">
-                    <h4 class="subtitle subtitle__dot-before subtitle__dot-after">professional people</h4>
-                    <h2 class="title">Our Leadership Team</h2>
-                    <p class="paragraph">We have experience working with large and small businesses and are ready to develop a targeted strategy and plan that’s just right for you.</p>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col col-33">
-                    <div class="team__card">
-                        <img src="<?php bloginfo('template_directory');?>/assets/images/091224867853_huge.png" alt="">
-                        <div class="team__card_info">
-                            <div class="team__name">Gina Bruno</div>
-                            <div class="team__work">WEB Designer</div>
-                            <div class="team__social">
-                                <a href="#" class="team__social_item team__social_add"></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col col-33">
-                    <div class="team__card">
-                        <img src="<?php bloginfo('template_directory');?>/assets/images/09220761288_xl-2015.png" alt="">
-                        <div class="team__card_info">
-                            <div class="team__name">David Ferry</div>
-                            <div class="team__work">CTO of Company</div>
-                            <div class="team__social">
-                                <a href="#" class="team__social_item team__social_add"></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col col-33">
-                    <div class="team__card">
-                        <img src="<?php bloginfo('template_directory');?>/assets/images/09233064538_xl-2015.png" alt="">
-                        <div class="team__card_info">
-                            <div class="team__name">Christina Tores</div>
-                            <div class="team__work">CEO of Company</div>
-                            <div class="team__social">
-                                <a href="#" class="team__social_item team__social_add"></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col col-33">
-                    <div class="team__card">
-                        <img src="<?php bloginfo('template_directory');?>/assets/images/09284358002_xl-2015.png" alt="">
-                        <div class="team__card_info">
-                            <div class="team__name">Regina Blackly </div>
-                            <div class="team__work">WEB Developer</div>
-                            <div class="team__social">
-                                <a href="#" class="team__social_item team__social_add"></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col col-33">
-                    <div class="team__card">
-                        <img src="<?php bloginfo('template_directory');?>/assets/images/091396486544_huge.png" alt="">
-                        <div class="team__card_info">
-                            <div class="team__name">Olivia Chee</div>
-                            <div class="team__work">General Manager</div>
-                            <div class="team__social">
-                                <a href="#" class="team__social_item team__social_add"></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col col-33">
-                    <div class="team__card">
-                        <img src="<?php bloginfo('template_directory');?>/assets/images/091414526951_huge.png" alt="">
-                        <div class="team__card_info">
-                            <div class="team__name">Robert Cooper</div>
-                            <div class="team__work">WEB Developer</div>
-                            <div class="team__social">
-                                <a href="#" class="team__social_item team__social_add"></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-
-</div>
-
-<?php
 get_footer();
