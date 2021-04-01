afb6c92621e587e81a45a47cc5da99d2e36545f6
Igor Pavlov <ip@geroy.ooo>
2021-04-01 14:31:35 +0300

    test menu js

--- assets/scripts/common.js
+++ assets/scripts/common.js
@@ -1,12 +1,30 @@
 jQuery(function($) {
+    $('.menu-item-has-children').on('mouseenter', function() {
+        $(this).find('.sub-menu').addClass('active');
+    });
+
     $('.menu-item-has-children').on('mouseout', function(e) {
         console.log(e.target);
         console.log(e.currentTarget);
+
+        if (!e.target.className.contains('sub-menu') && !e.currentTarget.className.contains('menu-item-has-children')) {
+            $(this).find('.sub-menu').removeClass('active'); 
+        }
+    });
+
+    $('.sub-menu').on('mouseleave', function(e) {
+        $(this).removeClass('active'); 
+
+        
+        
     })
 
     
-    $('.menu-item-has-children').on('mouseleave', function(e) {
-        console.log(e.target);
-        console.log(e.currentTarget);
-    })
+    // $('.menu-item-has-children').on('mouseleave', function(e) {
+    //     console.log(e.target);
+    //     console.log(e.currentTarget);
+
+        
+        
+    // })
 })
