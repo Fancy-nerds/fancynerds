c53a5141a6397f37ecef05ac45485f42a01cd68c
Igor Pavlov <ip@geroy.ooo>
2021-04-01 16:28:50 +0300

    header menu test

--- assets/scripts/common.js
+++ assets/scripts/common.js
@@ -1,23 +1,23 @@
 jQuery(function($) {
-    $('.menu-item-has-children').on('mouseenter', function() {
-        $(this).find('.sub-menu').addClass('active');
-    });
+    // $('.menu-item-has-children').on('mouseenter', function() {
+    //     $(this).find('.sub-menu').addClass('active');
+    // });
 
-    $('.menu-item-has-children').on('mouseout', function(e) {
-        console.log(e.target);
-        console.log(e.target.classList.contains('sub-menu'));
-        console.log('------------------------------------------------');
-        console.log(e.currentTarget);
-        console.log(e.currentTarget.classList.contains('menu-item-has-children'));
+    // $('.menu-item-has-children').on('mouseout', function(e) {
+    //     console.log(e.target);
+    //     console.log(e.target.classList.contains('sub-menu'));
+    //     console.log('------------------------------------------------');
+    //     console.log(e.currentTarget);
+    //     console.log(e.currentTarget.classList.contains('menu-item-has-children'));
 
-        if (!e.target.classList.contains('menu-item-has-children') && !e.currentTarget.classList.contains('menu-item-has-children')) {
-            $(this).find('.sub-menu').removeClass('active'); 
-        }
-    });
+    //     if (!e.target.classList.contains('menu-item-has-children') && !e.currentTarget.classList.contains('menu-item-has-children')) {
+    //         $(this).find('.sub-menu').removeClass('active'); 
+    //     }
+    // });
 
-    $('.sub-menu').on('mouseleave', function(e) {
-        $(this).removeClass('active'); 
-    })
+    // $('.sub-menu').on('mouseleave', function(e) {
+    //     $(this).removeClass('active'); 
+    // })
 
     
     // $('.menu-item-has-children').on('mouseleave', function(e) {
