dfd9027eab664b5801a3b563c0eb257b01b1adcc
Igor Pavlov <ip@geroy.ooo>
2021-04-05 13:22:49 +0300

    fix slider/slick styles

--- components/blocks/partners/partners.js
+++ components/blocks/partners/partners.js
@@ -1,30 +1,30 @@
-jQuery(function($) {
-    if ($(window).innerWidth() <= 768) {
-        $('.partners .row').slick({
-            dots: true,
-            arrows: false,
-            slidesToShow: 3,
-            slidesToScroll: 1,
-            swipeToSlide: 1,
-            infinite: true,
-            speed: 500,
-            swipeToSlide: false,
-            touchMove: false,
+// jQuery(function($) {
+//     if ($(window).innerWidth() <= 768) {
+//         $('.partners .row').slick({
+//             dots: true,
+//             arrows: false,
+//             slidesToShow: 3,
+//             slidesToScroll: 1,
+//             swipeToSlide: 1,
+//             infinite: true,
+//             speed: 500,
+//             swipeToSlide: false,
+//             touchMove: false,
         
-            responsive: [
-                {
-                    breakpoint: 568,
-                    settings: {
-                        slidesToShow: 2
-                    }
-                },
-                {
-                    breakpoint: 400,
-                    settings: {
-                        slidesToShow: 1
-                    }
-                }
-            ]
-        });
-    }
-});
+//             responsive: [
+//                 {
+//                     breakpoint: 568,
+//                     settings: {
+//                         slidesToShow: 2
+//                     }
+//                 },
+//                 {
+//                     breakpoint: 400,
+//                     settings: {
+//                         slidesToShow: 1
+//                     }
+//                 }
+//             ]
+//         });
+//     }
+// });
