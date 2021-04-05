acd9eb9387b1aac0f5e33078c2b24c72bddf53ab
Igor Pavlov <ip@geroy.ooo>
2021-04-05 11:33:21 +0300

    change slidetoggle to js

--- assets/scripts/common.js
+++ assets/scripts/common.js
@@ -11,14 +11,86 @@ jQuery(function($) {
         $(document).find('.header__menu').removeClass('active');
     });
 
-    $(document).on('click', '.menu-item-has-children', function(){
-        $(this).find('.sub-menu').slideToggle( { 
-            duration: 800,
-            easing: "linear",
-            complete: function(){
-              console.log("slideToggle completed");
-            },
-            queue: false // не ставим в очередь
+    // $(document).on('click', '.menu-item-has-children', function(){
+    //     $(this).find('.sub-menu').stop(true, true).slideToggle( { 
+    //         duration: 800,
+    //         easing: "linear",
+    //         complete: function(){
+    //           console.log("slideToggle completed");
+    //         },
+    //         queue: false // не ставим в очередь
+    //     });
+    // });
+
+    const elements = [...document.querySelectorAll('.menu-item-has-children')];
+    elements.forEach(accordion);
+
+    function findElements(object, element) {
+        const instance = object;
+        // element - это "вопрос", по которому происходит нажатие
+        instance.element = element;
+        // target - это "ответ", который должен "раскрываться"
+        instance.target = element.nextElementSibling;
+        console.log(element.nextElementSibling);
+        console.log(element);
+    }
+
+    function measureHeight(object) {
+        const instance = object;
+        // вычисляем высоту ответа
+        instance.height = object.target.firstElementChild.clientHeight;
+    }
+
+    function subscribe(instance) {
+        instance.element.addEventListener('click', (event) => {
+          // отменяем "действие по умолчанию"
+          event.preventDefault();
+          // меняем состояние аккордеона
+          changeElementStatus(instance);
         });
-    });
+        // если размер окна поменяется - измерим высоту ответа заново
+        window.addEventListener('resize', () => measureHeight(instance));
+    }
+
+    function changeElementStatus(instance) {
+        if (instance.isActive) {
+          hideElement(instance);
+        } else {
+          showElement(instance);
+        }
+    }
+
+    function hideElement(object) {
+        const instance = object;
+        const { target } = instance;
+        // обнуляем высоту ответа
+        target.style.height = null;
+        // делаем статус неактивным
+        instance.isActive = false;
+    }
+
+    function showElement(object) {
+        const instance = object;
+        const { target, height } = instance;
+        // задаем ответу сохраненную в measureHeight высоту
+        target.style.height = `${height}px`;
+        // делаем статус активным
+        instance.isActive = true;
+    }
+
+    function accordion(element) {
+        const instance = {};
+              
+        function init() {
+          // найдем вопрос и ответ
+          findElements(instance, element);
+          // измерим высоту ответа
+          measureHeight(instance);
+          // добавим логику нажатия на кнопку
+          subscribe(instance);
+        }
+        init();
+    }
+
+
 })
