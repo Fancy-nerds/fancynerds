1537364f79e898a1a7f3bbbb312bf96dfb8d56a6
Igor Pavlov <ip@geroy.ooo>
2021-04-05 13:37:52 +0300

    remove slick init js

--- components/blocks/team-slider/team-slider.js
+++ components/blocks/team-slider/team-slider.js
@@ -1,28 +1,28 @@
-jQuery(function($) {
-    $('.slider').slick({
-        dots: true,
-        arrows: false,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        swipeToSlide: 1,
-        infinite: true,
-        speed: 500,
-        swipeToSlide: false,
-        touchMove: false,
+// jQuery(function($) {
+//     $('.slider').slick({
+//         dots: true,
+//         arrows: false,
+//         slidesToShow: 3,
+//         slidesToScroll: 1,
+//         swipeToSlide: 1,
+//         infinite: true,
+//         speed: 500,
+//         swipeToSlide: false,
+//         touchMove: false,
     
-        responsive: [
-            {
-                breakpoint: 769,
-                settings: {
-                    slidesToShow: 2
-                }
-            },
-            {
-                breakpoint: 569,
-                settings: {
-                    slidesToShow: 1
-                }
-            }
-        ]
-    });
-});
+//         responsive: [
+//             {
+//                 breakpoint: 769,
+//                 settings: {
+//                     slidesToShow: 2
+//                 }
+//             },
+//             {
+//                 breakpoint: 569,
+//                 settings: {
+//                     slidesToShow: 1
+//                 }
+//             }
+//         ]
+//     });
+// });
--- src/scripts/blocks/plans.js
+++ src/scripts/blocks/plans.js
@@ -1,24 +1,24 @@
-jQuery(function($) {
-    if ($(window).innerWidth() <= 768) {
-        $('.plans__slider').slick({
-            dots: true,
-            arrows: false,
-            slidesToShow: 2,
-            slidesToScroll: 1,
-            swipeToSlide: 1,
-            infinite: true,
-            speed: 500,
-            swipeToSlide: false,
-            touchMove: false,
+// jQuery(function($) {
+//     if ($(window).innerWidth() <= 768) {
+//         $('.plans__slider').slick({
+//             dots: true,
+//             arrows: false,
+//             slidesToShow: 2,
+//             slidesToScroll: 1,
+//             swipeToSlide: 1,
+//             infinite: true,
+//             speed: 500,
+//             swipeToSlide: false,
+//             touchMove: false,
         
-            responsive: [
-                {
-                    breakpoint: 568,
-                    settings: {
-                        slidesToShow: 1
-                    }
-                }
-            ]
-        });
-    }
-});
+//             responsive: [
+//                 {
+//                     breakpoint: 568,
+//                     settings: {
+//                         slidesToShow: 1
+//                     }
+//                 }
+//             ]
+//         });
+//     }
+// });
