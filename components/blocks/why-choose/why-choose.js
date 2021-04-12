8344ad5c7d89958feffc925f1449758ff3af77a1
dmitriiii <dimitriy1988wl@gmail.com>
2021-04-12 17:51:05 +0300

    why choose js fix

--- components/blocks/why-choose/why-choose.js
+++ components/blocks/why-choose/why-choose.js
@@ -1,23 +1,23 @@
 jQuery(function($) {
-    function initBars() {
-        var bars = $(document).find('.bar');
+	function initBars() {
+		var bars = $(document).find('.bar');
 
-        $.each(bars, function() {
-            var percent = $(this).data('percent');
-            $(this).find('.bar__progress').css('width', percent);
-        });
-    }
-    $(document).ready(function() {
-        initBars();
-    })
-})
+		$.each(bars, function() {
+			var percent = $(this).data('percent');
+			$(this).find('.bar__progress').css('width', percent);
+		});
+	}
+	$(document).ready(function() {
+		initBars();
+	});
+	
+	$(document).on('scroll', function() {
+		var heightWindow = $(window).innerHeight();
+		var offsetBars = $('.progress-bars').offset().top;
+		var scrollTop = $(document).scrollTop();
 
-$(document).on('scroll', function() {
-    var heightWindow = $(window).innerHeight();
-    var offsetBars = $('.progress-bars').offset().top;
-    var scrollTop = $(document).scrollTop();
-
-    if (scrollTop > (offsetBars - heightWindow + 100)) {
-        initBars();
-    }
+		if (scrollTop > (offsetBars - heightWindow + 100)) {
+			initBars();
+		}
+	})
 })
