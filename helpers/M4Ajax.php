e35cc818cb1f799c661d2b78b30535ca6ca223d8
dmitriiii <dimitriy1988wl@gmail.com>
2021-04-19 12:16:30 +0300

    aff to bitrix24 conn

--- helpers/M4Ajax.php
+++ helpers/M4Ajax.php
@@ -3,8 +3,12 @@ require_once ( get_template_directory() . '/_libs/crest/crest.php' );
 
 class M4Ajax
 {
+	private $wpdb;
 	function __construct()
 	{
+		global $wpdb;
+		$this->wpdb = $wpdb;
+
 		#Form with leads
 		add_action('wp_ajax_nopriv_m4ajx_lead', array( $this, 'lead' ));
 		add_action('wp_ajax_m4ajx_lead', array( $this, 'lead' ));
@@ -31,6 +35,17 @@ class M4Ajax
 			$fields[$v['name']]=$v['value'];
 		}
 
+		#get First name / Last name / EMAIL / of affiliate
+		if( isset($fields[C_REST_AFF_ID]) && $fields[C_REST_AFF_ID] !== '' ){
+			$aff_users = $this->wpdb->prefix . "aff_affiliates";
+			$sql = $this->wpdb->prepare( "SELECT * FROM {$aff_users} WHERE affiliate_id=%d", $fields[C_REST_AFF_ID] );
+			$aff_user = $this->wpdb->get_results( $sql,ARRAY_A );
+			if(is_array($aff_user) && count($aff_user)>0){
+				$fields[C_REST_AFF_NAME] = $aff_user[0]['name'];
+				$fields[C_REST_AFF_EMAIL] = $aff_user[0]['email'];
+			}
+		}
+
 		CRest::call(
 			'crm.lead.add',
 			[
@@ -38,62 +53,8 @@ class M4Ajax
 			]
 		);
 
-		write_log(get_defined_vars());
+		// write_log(get_defined_vars());
 		
-
-		// $html = file_get_contents( "http://ip-api.com/json/".$ip."?fields=status,message,continent,continentCode,country,countryCode,region,regionName,city,district,zip,lat,lon,timezone,currency,isp,org,as,asname,reverse,mobile,proxy,query" );
-		
-		// append authorization data
-		// if (defined('CRM_AUTH'))
-		// {
-		// 	$postData['AUTH'] = CRM_AUTH;
-		// }
-		// else
-		// {
-		// 	$postData['LOGIN'] = CRM_LOGIN;
-		// 	$postData['PASSWORD'] = CRM_PASSWORD;
-		// }
-
-		// // open socket to CRM
-		// $fp = fsockopen("ssl://".CRM_HOST, CRM_PORT, $errno, $errstr, 30);
-		// if ($fp)
-		// {
-		// 	// prepare POST data
-		// 	$strPostData = '';
-		// 	foreach ($postData as $key => $value)
-		// 		$strPostData .= ($strPostData == '' ? '' : '&').$key.'='.urlencode($value);
-
-		// 	// prepare POST headers
-		// 	$str = "POST ".CRM_PATH." HTTP/1.0\r\n";
-		// 	$str .= "Host: ".CRM_HOST."\r\n";
-		// 	$str .= "Content-Type: application/x-www-form-urlencoded\r\n";
-		// 	$str .= "Content-Length: ".strlen($strPostData)."\r\n";
-		// 	$str .= "Connection: close\r\n\r\n";
-
-		// 	$str .= $strPostData;
-
-		// 	// send POST to CRM
-		// 	fwrite($fp, $str);
-
-		// 	// get CRM headers
-		// 	$result = '';
-		// 	while (!feof($fp))
-		// 	{
-		// 		$result .= fgets($fp, 128);
-		// 	}
-		// 	fclose($fp);
-
-		// 	// cut response headers
-		// 	$response = explode("\r\n\r\n", $result);
-
-		// 	$output = '<pre>'.print_r($response[1], 1).'</pre>';
-		// }
-		// else
-		// {
-		// 	$output = 'Connection Failed! '.$errstr.' ('.$errno.')';
-		// }
-
-		// $arrr['html'] = $output;
 		$arrr['success'] = true;
  		echo json_encode( $arrr );
 		exit();
