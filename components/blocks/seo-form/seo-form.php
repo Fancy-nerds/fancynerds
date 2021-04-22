3e00ad2451490a91fa5577e063ec332d6fd2686c
Igor Pavlov <ip@geroy.ooo>
2021-04-22 16:12:20 +0300

    add blocks services-list & seo-form

---
+++ components/blocks/seo-form/seo-form.php
@@ -0,0 +1,24 @@
+<div class="seo-form">
+    <div class="container">
+        <div class="seo-form__wrapper">
+            <div class="seo-form__elements"></div>
+            <div class="seo-form__content">
+                <h6 class="seo-form__title">Know your SEO Score!</h6>
+                <form class="seo-form__form">
+                    <span class="seo-form__item">
+                        <input type="text" placeholder="Your Website URL" />
+                    </span>
+                    <span class="seo-form__item">
+                        <input type="text" placeholder="Email">
+                    </span>
+
+                    <a href="#" class="button button--orange button--image">Start Now
+                        <span>
+                            <i class="flaticon-right-arrow-1"></i>
+                        </span>
+                    </a>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
