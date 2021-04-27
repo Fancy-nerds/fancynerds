9b1a568bbd0feab3a18df8f10ac91d2fbcef745a
Delave-las-Kure <t1evgeny93@gmail.com>
2021-04-28 00:20:25 +0300

    made team grid responsive #3

--- components/shortcodes/team/layout1.php
+++ components/shortcodes/team/layout1.php
@@ -1,26 +1,24 @@
 <?php
-if (!defined( 'ABSPATH' ))
-exit('Direct access not allowed!');
+if (!defined('ABSPATH'))
+	exit('Direct access not allowed!');
 
-if( is_array($user_ids) && count($user_ids)>0 ):?>
-	<div class="team__slider slider row">
+if (is_array($user_ids) && count($user_ids) > 0) : ?>
+	<div class="team__grid">
 		<?php
-		foreach ($user_ids as $k=>$v):
+		foreach ($user_ids as $k => $v) :
 			$all_meta = get_user_meta($v);
-			if(!$all_meta)
+			if (!$all_meta)
 				continue; ?>
-			<div class="col col-33">
-				<div class="team__card">
-					<?php 
-					if($all_meta['user_photo'][0]):
-						echo M4Helpers::getImgHtml([ 'img_id'=>$all_meta['user_photo'][0], 'size'=>'team', 'class'=>'image--rounded']);
-					endif; ?>
-					<div class="team__card_info">
-						<div class="team__name"><?= $all_meta['first_name'][0] . ' ' . $all_meta['last_name'][0]; ?></div>
-						<div class="team__work"><?= $all_meta[$description][0]; ?></div>
-						<div class="team__social">
-							<a href="#" class="team__social_item team__social_add"></a>
-						</div>
+			<div class="team__card">
+				<?php
+				if ($all_meta['user_photo'][0]) :
+					echo M4Helpers::getImgHtml(['img_id' => $all_meta['user_photo'][0], 'size' => 'team', 'class' => 'image--rounded']);
+				endif; ?>
+				<div class="team__card_info">
+					<div class="team__name"><?= $all_meta['first_name'][0] . ' ' . $all_meta['last_name'][0]; ?></div>
+					<div class="team__work"><?= $all_meta[$description][0]; ?></div>
+					<div class="team__social">
+						<a href="#" class="team__social_item team__social_add"></a>
 					</div>
 				</div>
 			</div>
