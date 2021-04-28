320b197420e7fce2c8e4a7267ac682d6dc88eb23
Delave-las-Kure <t1evgeny93@gmail.com>
2021-04-28 21:46:41 +0300

    made counters responsive #3

--- components/blocks/counters/counters.php
+++ components/blocks/counters/counters.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Services Block Template.
  *
@@ -8,36 +9,37 @@
  * @param   (int|string) $post_id The post ID this block is saved to.
  */
 
-extract(M4Helpers::prepBlock($block,[
-	'top'=>'-130px',
-]));
+extract(M4Helpers::prepBlock($block));
 
 $counters = get_field('counters');
 // $style.= " top:-130px;";
 /* Render screenshot for preview */
-if (get_field('is_example',$block['id'])) :
-	echo "<img src='".get_template_directory_uri()."/components/blocks/".$block['title']."/".$block['title'].".png'/>";
+if (get_field('is_example', $block['id'])) :
+	echo "<img src='" . get_template_directory_uri() . "/components/blocks/" . $block['title'] . "/" . $block['title'] . ".png'/>";
 	return;
 endif;
 
-if (is_array($counters) && count($counters)>0):?>
-	<div class="<?= $className; ?>" id="<?php echo esc_attr($id); ?>" <?= $style;?>>
+if (is_array($counters) && count($counters) > 0) : ?>
+	<div class="<?= $className; ?>" id="<?php echo esc_attr($id); ?>" <?= $style; ?>>
 		<div class="container">
-			<div class="counters__bg"></div>
-			<div class="row">
-				<?php
-				foreach ($counters as $counter): ?>
-					<div class="col col-25">
-						<div class="counters__item">
-							<div class="subtitle subtitle__dot-before"><?= $counter['subtitle'];?></div>
-							<div class="counters__count">
-								<span data-counter="<?= $counter['to'];?>">0</span>+
+			<div class="counters__inner">
+				<div class="counters__bg"></div>
+				<div class="row">
+					<?php
+					foreach ($counters as $counter) : ?>
+						<div class="col col-25">
+							<div class="counters__item">
+								<div class="subtitle subtitle__dot-before"><?= $counter['subtitle']; ?></div>
+								<div class="counters__count">
+									<span data-counter="<?= $counter['to']; ?>">0</span>+
+								</div>
 							</div>
 						</div>
-					</div>
-				<?php
-				endforeach; ?>
+					<?php
+					endforeach; ?>
+				</div>
 			</div>
+
 		</div>
 	</div>
 <?php
