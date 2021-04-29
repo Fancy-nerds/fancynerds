65b1f1bdf1952d8330831bc8bb6c87af7bd7dc8b
Delave-las-Kure <t1evgeny93@gmail.com>
2021-04-29 22:43:46 +0300

    more overflow fixes

--- footer.php
+++ footer.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying the footer
  *
@@ -17,170 +18,171 @@
 
 </main>
 
-		<footer class="footer">
-			<div class="container">
-				<div class="row">
-					<div class="col col-25">
-						<div class="footer__column">
-							<h6 class="footer__title">Services</h6>
+<footer class="footer">
+	<div class="container">
+		<div class="row">
+			<div class="col col-25">
+				<div class="footer__column">
+					<h6 class="footer__title">Services</h6>
 
-							<ul class="footer__menu">
-								<li class="footer__item">
-									<a href="#" class="link">SEO for Small Business</a>
-								</li>
-								<li class="footer__item">
-									<a href="#" class="link">SEO for Local Services</a>
-								</li>
-								<li class="footer__item">
-									<a href="#" class="link">Enterprise SEO</a>
-								</li>
-								<li class="footer__item">
-									<a href="#" class="link">National SEO</a>
-								</li>
-								<li class="footer__item">
-									<a href="#" class="link">International SEO</a>
-								</li>
-							</ul>
-						</div>
-					</div>
-					<div class="col col-25">
-						<div class="footer__column">
-							<h6 class="footer__title">Learn</h6>
-
-							<ul class="footer__menu">
-								<li class="footer__item">
-									<a href="#" class="link">Why SEO Matters</a>
-								</li>
-								<li class="footer__item">
-									<a href="#" class="link">How to Choose an Agency</a>
-								</li>
-								<li class="footer__item">
-									<a href="#" class="link">Keyword Research Explained</a>
-								</li>
-								<li class="footer__item">
-									<a href="#" class="link">On Page SEO Explained</a>
-								</li>
-								<li class="footer__item">
-									<a href="#" class="link">Off Page SEO Explained</a>
-								</li>
-							</ul>
-						</div>
-					</div>
-					<div class="col col-25">
-						<div class="footer__column">
-							<h6 class="footer__title">Company</h6>
-
-							<ul class="footer__menu">
-								<li class="footer__item">
-									<a href="#" class="link">About Company</a>
-								</li>
-								<li class="footer__item">
-									<a href="#" class="link">IFor Customers</a>
-								</li>
-								<li class="footer__item">
-									<a href="#" class="link">SEO Blog &amp; News</a>
-								</li>
-								<li class="footer__item">
-									<a href="#" class="link">Careers &amp; Reviews</a>
-								</li>
-								<li class="footer__item">
-									<a href="#" class="link">Sitemap</a>
-								</li>
-							</ul>
-						</div>
-					</div>
-					<div class="col col-25 subscribe-col">
-						<div class="footer__column">
-							<h6 class="footer__title">Subscribe</h6>
-
-							<p class="footer__text">Follow our newsletter to stay updated about agency.</p>
-
-							<form class="form form__subscribe">
-								<div class="form__group">
-									<input placeholder="Your mail" class="form__control">
-									<button class="form__button button--rounded">
-										<i class="flaticon-send"></i>
-									</button>
-								</div>
-							</form>
-						</div>
-					</div>
+					<ul class="footer__menu">
+						<li class="footer__item">
+							<a href="#" class="link">SEO for Small Business</a>
+						</li>
+						<li class="footer__item">
+							<a href="#" class="link">SEO for Local Services</a>
+						</li>
+						<li class="footer__item">
+							<a href="#" class="link">Enterprise SEO</a>
+						</li>
+						<li class="footer__item">
+							<a href="#" class="link">National SEO</a>
+						</li>
+						<li class="footer__item">
+							<a href="#" class="link">International SEO</a>
+						</li>
+					</ul>
 				</div>
 			</div>
-		</footer>
+			<div class="col col-25">
+				<div class="footer__column">
+					<h6 class="footer__title">Learn</h6>
 
-	</div><!-- #page -->
+					<ul class="footer__menu">
+						<li class="footer__item">
+							<a href="#" class="link">Why SEO Matters</a>
+						</li>
+						<li class="footer__item">
+							<a href="#" class="link">How to Choose an Agency</a>
+						</li>
+						<li class="footer__item">
+							<a href="#" class="link">Keyword Research Explained</a>
+						</li>
+						<li class="footer__item">
+							<a href="#" class="link">On Page SEO Explained</a>
+						</li>
+						<li class="footer__item">
+							<a href="#" class="link">Off Page SEO Explained</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div class="col col-25">
+				<div class="footer__column">
+					<h6 class="footer__title">Company</h6>
 
-	<div id="myModal" class="modal">
-	  <!-- Modal content -->
-	  <div class="modal-content">
-	    <div class="modal-header clearfix">
-	    	<p class="modal-header__title">Contact our team!</p>
-	      <span class="close">&times;</span>
-	    </div>
-	    <div class="modal-body">
-	    	<form class="container form-ajx">
-	    		<div class="row">
-			    	<input name="TITLE" value="Fancy-nerds.com Contact us Form" type="hidden"/>
-			    	<?= (isset($_COOKIE['wp_affiliates']) ? "<input name='".C_REST_AFF_ID."' value='".$_COOKIE['wp_affiliates']."' type='hidden'/>" : ""); ?>
-	    			<div class="col-50 col">
-	    				<label>First Name*</label>
-			    		<input name="NAME" value="" class="form__control" required/>
-	    			</div>
-	    			<div class="col-50 col">
-	    				<label>Last Name*</label>
-			    		<input name="LAST_NAME" value="" class="form__control" required/>
-	    			</div>
+					<ul class="footer__menu">
+						<li class="footer__item">
+							<a href="#" class="link">About Company</a>
+						</li>
+						<li class="footer__item">
+							<a href="#" class="link">IFor Customers</a>
+						</li>
+						<li class="footer__item">
+							<a href="#" class="link">SEO Blog &amp; News</a>
+						</li>
+						<li class="footer__item">
+							<a href="#" class="link">Careers &amp; Reviews</a>
+						</li>
+						<li class="footer__item">
+							<a href="#" class="link">Sitemap</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div class="col col-25 subscribe-col">
+				<div class="footer__column">
+					<h6 class="footer__title">Subscribe</h6>
 
-	    			<div class="col-50 col">
-	    				<label>Email*</label>
-			    		<input name="EMAIL" value="" class="form__control" required/>
-	    			</div>
+					<p class="footer__text">Follow our newsletter to stay updated about agency.</p>
 
-	    			<div class="col-50 col">
-	    				<label>Phone</label>
-			    		<input name="PHONE" value="" class="form__control"/>
-	    			</div>
-
-	    			<div class="col col-100">
-	    				Comments: <textarea name="COMMENTS" class="form__control"></textarea><br />
-	    				<button class="button button--small button--orange modal-body__btn" >Send</button>
-	    				<p class="form-ajx__result"></p>
-	    			</div>
-	    		</div>
-	    	</form>
-
-	    </div>
-	  </div>
+					<form class="form form__subscribe">
+						<div class="form__group">
+							<input placeholder="Your mail" class="form__control">
+							<button class="form__button button--rounded">
+								<i class="flaticon-send"></i>
+							</button>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
 	</div>
+</footer>
 
+</div><!-- #page -->
+
+<div id="myModal" class="modal">
+	<!-- Modal content -->
+	<div class="modal-content">
+		<div class="modal-header clearfix">
+			<p class="modal-header__title">Contact our team!</p>
+			<span class="close">&times;</span>
+		</div>
+		<div class="modal-body">
+			<form class="container form-ajx">
+				<div class="row">
+					<input name="TITLE" value="Fancy-nerds.com Contact us Form" type="hidden" />
+					<?= (isset($_COOKIE['wp_affiliates']) ? "<input name='" . C_REST_AFF_ID . "' value='" . $_COOKIE['wp_affiliates'] . "' type='hidden'/>" : ""); ?>
+					<div class="col-50 col">
+						<label>First Name*</label>
+						<input name="NAME" value="" class="form__control" required />
+					</div>
+					<div class="col-50 col">
+						<label>Last Name*</label>
+						<input name="LAST_NAME" value="" class="form__control" required />
+					</div>
+
+					<div class="col-50 col">
+						<label>Email*</label>
+						<input name="EMAIL" value="" class="form__control" required />
+					</div>
+
+					<div class="col-50 col">
+						<label>Phone</label>
+						<input name="PHONE" value="" class="form__control" />
+					</div>
+
+					<div class="col col-100">
+						Comments: <textarea name="COMMENTS" class="form__control"></textarea><br />
+						<button class="button button--small button--orange modal-body__btn">Send</button>
+						<p class="form-ajx__result"></p>
+					</div>
+				</div>
+			</form>
+
+		</div>
+	</div>
+</div>
+</div>
 <script>
-// Get the modal
-var modal = document.getElementById("myModal");
+	// Get the modal
+	var modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+	// Get the button that opens the modal
+	var btn = document.getElementById("myBtn");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+	// Get the <span> element that closes the modal
+	var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
+	// When the user clicks the button, open the modal
+	btn.onclick = function() {
+		modal.style.display = "block";
+	}
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+	// When the user clicks on <span> (x), close the modal
+	span.onclick = function() {
+		modal.style.display = "none";
+	}
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+	// When the user clicks anywhere outside of the modal, close it
+	window.onclick = function(event) {
+		if (event.target == modal) {
+			modal.style.display = "none";
+		}
+	}
 </script>
 
 </body>
+
 </html>
--- header.php
+++ header.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The header for our theme
  *
@@ -12,15 +13,16 @@
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
+
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
 	<?php
-		if (pll_current_language() == 'ru') { ?>
-			<link rel="stylesheet" id="fontMonserrat-css" href="<?= get_template_directory_uri().'/assets/styles/fontMonserrat.css'?>" media="all">
-		<?php } 
+	if (pll_current_language() == 'ru') { ?>
+		<link rel="stylesheet" id="fontMonserrat-css" href="<?= get_template_directory_uri() . '/assets/styles/fontMonserrat.css' ?>" media="all">
+	<?php }
 	?>
 
 	<?php wp_head(); ?>
@@ -28,82 +30,111 @@
 	<style>
 		/* The Modal (background) */
 		.modal {
-		  display: none; /* Hidden by default */
-		  position: fixed; /* Stay in place */
-		  z-index: 9999; /* Sit on top */
-		  padding-top: 100px; /* Location of the box */
-		  left: 0;
-		  top: 0;
-		  width: 100%; /* Full width */
-		  height: 100%; /* Full height */
-		  overflow: auto; /* Enable scroll if needed */
-		  background-color: rgb(0,0,0); /* Fallback color */
-		  background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+			display: none;
+			/* Hidden by default */
+			position: fixed;
+			/* Stay in place */
+			z-index: 9999;
+			/* Sit on top */
+			padding-top: 100px;
+			/* Location of the box */
+			left: 0;
+			top: 0;
+			width: 100%;
+			/* Full width */
+			height: 100%;
+			/* Full height */
+			overflow: auto;
+			/* Enable scroll if needed */
+			background-color: rgb(0, 0, 0);
+			/* Fallback color */
+			background-color: rgba(0, 0, 0, 0.4);
+			/* Black w/ opacity */
 		}
 
 		/* Modal Content */
 		.modal-content {
-		  position: relative;
-		  background-color: #fefefe;
-		  margin: auto;
-		  padding: 0;
-		  border: 1px solid #888;
-		  width: 100%;
-		  max-width: 500px;
-		  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2),0 6px 20px 0 rgba(0,0,0,0.19);
-		  -webkit-animation-name: animatetop;
-		  -webkit-animation-duration: 0.4s;
-		  animation-name: animatetop;
-		  animation-duration: 0.4s;
-		  border-radius: 5px;
+			position: relative;
+			background-color: #fefefe;
+			margin: auto;
+			padding: 0;
+			border: 1px solid #888;
+			width: 100%;
+			max-width: 500px;
+			box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+			-webkit-animation-name: animatetop;
+			-webkit-animation-duration: 0.4s;
+			animation-name: animatetop;
+			animation-duration: 0.4s;
+			border-radius: 5px;
 		}
 
 		/* Add Animation */
 		@-webkit-keyframes animatetop {
-		  from {top:-300px; opacity:0} 
-		  to {top:0; opacity:1}
+			from {
+				top: -300px;
+				opacity: 0
+			}
+
+			to {
+				top: 0;
+				opacity: 1
+			}
 		}
 
 		@keyframes animatetop {
-		  from {top:-300px; opacity:0}
-		  to {top:0; opacity:1}
+			from {
+				top: -300px;
+				opacity: 0
+			}
+
+			to {
+				top: 0;
+				opacity: 1
+			}
 		}
 
-		.modal-header{
+		.modal-header {
 			position: relative;
 		}
-		.modal-header__title{
+
+		.modal-header__title {
 			font-size: 30px;
 			text-align: center;
 			margin-bottom: 10px;
 			padding-top: 15px;
 		}
+
 		/* The Close Button */
 		.close {
 			position: absolute;
 			right: 15px;
 			top: 5px;
-		  color: red;
-		  /* float: right; */
-		  font-size: 28px;
-		  font-weight: bold;
+			color: red;
+			/* float: right; */
+			font-size: 28px;
+			font-weight: bold;
 		}
 
 		.close:hover,
 		.close:focus {
-		  color: #000;
-		  text-decoration: none;
-		  cursor: pointer;
+			color: #000;
+			text-decoration: none;
+			cursor: pointer;
 		}
 
-		.modal-body {padding: 25px;}
-		.modal-body__btn{
+		.modal-body {
+			padding: 25px;
+		}
+
+		.modal-body__btn {
 			display: block;
 			margin-left: auto;
 			margin-top: 15px;
 		}
+
 		.modal input,
-		.modal textarea{
+		.modal textarea {
 			width: 100%;
 			resize: none;
 		}
@@ -113,59 +144,59 @@
 </head>
 
 <body <?php body_class(); ?>>
-<?php wp_body_open(); 
-$add_clss = 'home';
-if(is_page_template('tpl-team.php'))
-	$add_clss = 'team';
-if(is_page_template('tpl-about.php'))
-	$add_clss = 'about';
-if(is_page_template('tpl-service.php'))
-	$add_clss = 'service';
-?>
+	<?php wp_body_open();
+	$add_clss = 'home';
+	if (is_page_template('tpl-team.php'))
+		$add_clss = 'team';
+	if (is_page_template('tpl-about.php'))
+		$add_clss = 'about';
+	if (is_page_template('tpl-service.php'))
+		$add_clss = 'service';
+	?>
 
-  
 
-<div id="page" class="site <?= $add_clss;?>">
 
-	<header class="header">
-		<div class="header__main">
-			<a href="<?= home_url(); ?>" class="header__logo">
-				<img width="89" height="80" src="<?php bloginfo('template_directory');?>/assets/images/logo-fancy-nerds.svg">
-			</a>
+	<div id="page" class="site <?= $add_clss; ?>">
 
-			<div class="header__menu">
-				<div class="header__close">
-					<a href="#" class="header__close_action">
-						<i class="flaticon-close"></i>
-					</a>
+		<header class="header">
+			<div class="header__main">
+				<a href="<?= home_url(); ?>" class="header__logo">
+					<img width="89" height="80" src="<?php bloginfo('template_directory'); ?>/assets/images/logo-fancy-nerds.svg">
+				</a>
+
+				<div class="header__menu">
+					<div class="header__close">
+						<a href="#" class="header__close_action">
+							<i class="flaticon-close"></i>
+						</a>
+					</div>
+					<?php wp_nav_menu(array(
+						'theme_location'  => 'menu-1',
+						'menu_class'      => 'menu',
+						'container_class' => 'header__menu_container',
+					)); ?>
 				</div>
-				<?php wp_nav_menu( array(
-					'theme_location'  => 'menu-1',
-					'menu_class'      => 'menu',
-					'container_class' => 'header__menu_container',
-				) ); ?>
-			</div>
 
-			<div class="header__actions">
-				<ul class="header__flags"><?php pll_the_languages(['show_flags' => 1,'show_names' => 0]);?></ul>
-				<button class="button button--blue-dark header__contact-us" id="myBtn">
-					<span>Contact Us</span>
-					<img src="<?php bloginfo('template_directory');?>/assets/images/telephone.svg" width="25" height="25">
+				<div class="header__actions">
+					<ul class="header__flags"><?php pll_the_languages(['show_flags' => 1, 'show_names' => 0]); ?></ul>
+					<button class="button button--blue-dark header__contact-us" id="myBtn">
+						<span>Contact Us</span>
+						<img src="<?php bloginfo('template_directory'); ?>/assets/images/telephone.svg" width="25" height="25">
 
-				</button>
-				<a href="#" class="header__mobile-menu">
-					<svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 24.75 24.75" style="enable-background:new 0 0 24.75 24.75;" xml:space="preserve">
-						<g>
-							<path d="M0,3.875c0-1.104,0.896-2,2-2h20.75c1.104,0,2,0.896,2,2s-0.896,2-2,2H2C0.896,5.875,0,4.979,0,3.875z M22.75,10.375H2
+					</button>
+					<a href="#" class="header__mobile-menu">
+						<svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 24.75 24.75" style="enable-background:new 0 0 24.75 24.75;" xml:space="preserve">
+							<g>
+								<path d="M0,3.875c0-1.104,0.896-2,2-2h20.75c1.104,0,2,0.896,2,2s-0.896,2-2,2H2C0.896,5.875,0,4.979,0,3.875z M22.75,10.375H2
 								c-1.104,0-2,0.896-2,2c0,1.104,0.896,2,2,2h20.75c1.104,0,2-0.896,2-2C24.75,11.271,23.855,10.375,22.75,10.375z M22.75,18.875H2
 								c-1.104,0-2,0.896-2,2s0.896,2,2,2h20.75c1.104,0,2-0.896,2-2S23.855,18.875,22.75,18.875z"></path>
-						</g>
-					</svg>
+							</g>
+						</svg>
 
-					<div class="header__overlay"></div>
-				</a>
+						<div class="header__overlay"></div>
+					</a>
+				</div>
 			</div>
-		</div>
-	</header>
-
-	<main class="main">
+		</header>
+		<div class="page-overflow">
+			<main class="main">
