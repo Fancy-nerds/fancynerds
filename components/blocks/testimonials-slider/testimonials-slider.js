1fc7d41d1199fdcdd2c0780995d9310f0c4c818e
Delave-las-Kure <t1evgeny93@gmail.com>
2021-05-07 00:01:35 +0300

    [b]testimonials-slider: prettify js

--- components/blocks/testimonials-slider/testimonials-slider.js
+++ components/blocks/testimonials-slider/testimonials-slider.js
@@ -1,14 +1,14 @@
-jQuery(function($) {
-    var swiper = new Swiper('.swiper-container', {
-    		autoHeight: true, //enable auto height
-        spaceBetween: 30,
-        autoplay: {
-			   delay: 3000,
-			 	},
-        loop: true,
-        navigation: {
-            nextEl: '.testimonials-slider__button-next',
-            prevEl: '.testimonials-slider__button-prev',
-        },
-    });
+jQuery(function ($) {
+  var swiper = new Swiper(".testimonials-slider__slider .swiper-container", {
+    autoHeight: true, //enable auto height
+    spaceBetween: 30,
+    autoplay: {
+      delay: 3000,
+    },
+    loop: true,
+    navigation: {
+      nextEl: ".testimonials-slider__button-next",
+      prevEl: ".testimonials-slider__button-prev",
+    },
+  });
 });
