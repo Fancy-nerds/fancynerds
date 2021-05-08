6632953a9f8b0134a7eaa9850a9ce545146e82e5
Delave-las-Kure <t1evgeny93@gmail.com>
2021-05-08 14:58:40 +0300

    [b]jumb__inner: made responsive #3

--- template-parts/jumbotron__inner.php
+++ template-parts/jumbotron__inner.php
@@ -1,34 +1,36 @@
-<?php wp_enqueue_style( 'fancynerds-part-jumbo-css' );
+<?php wp_enqueue_style('fancynerds-part-jumbo-css');
 
 global $post;
 $breads = [
 	[
-		'href'=>get_home_url(),
-		'title'=>get_the_title( get_option('page_on_front')),
+		'href' => get_home_url(),
+		'title' => get_the_title(get_option('page_on_front')),
 	]
 ];
 
 #If on child page
-if ( is_page() && $post->post_parent )
-$breads[]=[
-	'href' => get_permalink( $post->post_parent ),
-	'title' => get_the_title( $post->post_parent )
-]; ?>
+if (is_page() && $post->post_parent)
+	$breads[] = [
+		'href' => get_permalink($post->post_parent),
+		'title' => get_the_title($post->post_parent)
+	]; ?>
 
 <section class="section jumbotron__inner">
+	<div class="jumbotron__inner__bg">
+		<img src="<?php bloginfo('template_directory');?>/assets/images/bg-page-header.jpg" width="1920" height="300">
+	</div>
 	<div class="container">
-		<div class="row">
-			<div class="heading">
-				<h1 class="title"><?= $args['title']; ?></h1>
-				<div class="breadcrumbs">
-					<?php
-					foreach ($breads as $bread): ?>
-						<a class="breadcrumbs__item active" href="<?= $bread['href']; ?>"><?= $bread['title']; ?></a> >
-					<?php
-					endforeach; ?>
-					<span class="breadcrumbs__item"><?= $args['title']; ?></span>
-				</div>
+		<div class="heading">
+			<h1 class="title"><?= $args['title']; ?></h1>
+			<div class="breadcrumbs">
+				<?php
+				foreach ($breads as $bread) : ?>
+					<a class="breadcrumbs__item active" href="<?= $bread['href']; ?>"><?= $bread['title']; ?></a> >
+				<?php
+				endforeach; ?>
+				<span class="breadcrumbs__item"><?= $args['title']; ?></span>
 			</div>
 		</div>
+
 	</div>
 </section>
