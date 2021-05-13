685ec7df2769556aa92c9561cab7da892c0e2d6f
Delave-las-Kure <t1evgeny93@gmail.com>
2021-05-13 23:02:55 +0300

    [b]testimonials-slider: removed jquery

--- components/blocks/testimonials-slider/testimonials-slider.js
+++ components/blocks/testimonials-slider/testimonials-slider.js
@@ -1,14 +1,21 @@
-jQuery(function ($) {
-  var swiper = new Swiper(".testimonials-slider__slider .swiper-container", {
-    autoHeight: true, //enable auto height
-    spaceBetween: 30,
-    autoplay: {
-      delay: 3000,
-    },
-    loop: true,
-    navigation: {
-      nextEl: ".testimonials-slider__button-next",
-      prevEl: ".testimonials-slider__button-prev",
-    },
-  });
-});
+(function () {
+  document
+    .querySelectorAll(".testimonials-slider__slider .swiper-container")
+    .forEach((sliderEl) => {
+      new Swiper(
+        sliderEl,
+        {
+          autoHeight: true, //enable auto height
+          spaceBetween: 30,
+          autoplay: {
+            delay: 3000,
+          },
+          loop: true,
+          navigation: {
+            nextEl: sliderEl.querySelector(".testimonials-slider__button-next"),
+            prevEl: sliderEl.querySelector(".testimonials-slider__button-prev"),
+          },
+        }
+      );
+    });
+})();
