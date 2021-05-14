044448b106b9f4316516ea6eabb911d4a0b595b0
Delave-las-Kure <t1evgeny93@gmail.com>
2021-05-14 18:41:55 +0300

    [b]testimonials-slider: fixed slider prev/next

--- src/scripts/blocks/testimonials-slider.js
+++ src/scripts/blocks/testimonials-slider.js
@@ -1,21 +1,20 @@
-(function () {
+function initTestimonialsSlider() {
   document
     .querySelectorAll(".testimonials-slider__slider .swiper-container")
     .forEach((sliderEl) => {
-      new Swiper(
-        sliderEl,
-        {
-          autoHeight: true, //enable auto height
-          spaceBetween: 30,
-          autoplay: {
-            delay: 3000,
-          },
-          loop: true,
-          navigation: {
-            nextEl: sliderEl.querySelector(".testimonials-slider__button-next"),
-            prevEl: sliderEl.querySelector(".testimonials-slider__button-prev"),
-          },
-        }
-      );
+      let container = sliderEl.closest('.testimonials-slider')
+      new Swiper(sliderEl, {
+        autoHeight: true, //enable auto height
+        spaceBetween: 30,
+        autoplay: {
+          delay: 3000,
+        },
+        loop: true,
+        navigation: {
+          nextEl: container.querySelector(".testimonials-slider__button-next"),
+          prevEl: container.querySelector(".testimonials-slider__button-prev"),
+        },
+      });
     });
-})();
+}
+initTestimonialsSlider()
