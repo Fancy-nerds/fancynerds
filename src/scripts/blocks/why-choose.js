64fa8f532185a29bf423949252d6bb9ea9453e07
Delave-las-Kure <t1evgeny93@gmail.com>
2021-05-14 18:03:53 +0300

    [b]why-choose: more encapsulation

--- src/scripts/blocks/why-choose.js
+++ src/scripts/blocks/why-choose.js
@@ -1,24 +1,26 @@
 (function () {
   function initBars() {
-    document.querySelectorAll(".progress-bars").forEach((pBars) => {
-      let isIntersecting = false;
-      const observer = new IntersectionObserver(
-        ([entry]) => {
-          if (entry.isIntersecting && !isIntersecting) {
-            isIntersecting = true;
-            pBars.querySelectorAll(".bar").forEach((bar, ind) => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting && !entry.target.classList.contains('progress-bars--intersected')) {
+            entry.target.classList.add('progress-bars--intersected')
+            entry.target.querySelectorAll(".bar").forEach((bar, ind) => {
               setTimeout(() => {
                 bar.querySelector(".bar__progress").style.width =
                   bar.dataset.percent;
               }, ind * 300);
             });
           }
-        },
-        {
-          root: null,
-          threshold: 0.65,
-        }
-      );
+        });
+      },
+      {
+        root: null,
+        threshold: 0.65,
+      }
+    );
+    document.querySelectorAll(".progress-bars").forEach((pBars) => {
+      if (pBars.classList.contains('progress-bars--initiated')) return;
       observer.observe(pBars);
     });
   }
