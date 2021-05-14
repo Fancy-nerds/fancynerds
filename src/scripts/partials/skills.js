b458f9f84b90cd97213803f414cc8a7e64f61a72
Delave-las-Kure <t1evgeny93@gmail.com>
2021-05-14 19:16:02 +0300

    [b]skills: removed jquery

--- src/scripts/partials/skills.js
+++ src/scripts/partials/skills.js
@@ -1,29 +1,49 @@
-jQuery(function($) {
-	function initSkills() {
-		var skills = $(document).find('.skills__item');
-        var heightWindow = $(window).innerHeight();
-		var offsetSkills = $('.skills').offset().top;
-		var scrollTop = $(document).scrollTop();
-
-		if (scrollTop > (offsetSkills - heightWindow + 120)) {
-			init();
-		}
-
-        function init() {
-            $.each(skills, function() {
-                var percent = $(this).data('percent');
-                $(this).find('.donut .donut-segment').attr('stroke-dasharray', percent + ' ' + (100 - percent));
-                $(this).find('.donut .donut-segment').attr('stroke-linecap', 'round');
-                $(this).find('.skills__round span').text(percent);
-            });
+function initSkills() {
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (
+          entry.isIntersecting &&
+          !entry.target.classList.contains("skills--intersected")
+        ) {
+          entry.target.classList.add("skills--intersected");
+          entry.target.querySelectorAll(".skills__item").forEach((skillEl) => {
+            const percent = skillEl.dataset.percent;
+            const segmentEl = skillEl.querySelector(".donut .donut-segment");
+            const spanEl = skillEl.querySelector(".skills__round span");
+            segmentEl.setAttribute(
+              "stroke-dasharray",
+              percent + " " + (100 - percent)
+            );
+            segmentEl.setAttribute("stroke-linecap", "round");
+            animateValue(spanEl, 0, percent, 600);
+          });
         }
-	}
+      });
+    },
+    {
+      root: null,
+      threshold: 0.65,
+    }
+  );
+  document.querySelectorAll(".skills").forEach((skillsEl) => {
+    if (skillsEl.classList.contains("skills--initiated")) return;
+    skillsEl.classList.add("skills--initiated");
+    observer.observe(skillsEl);
+  });
 
-	$(document).ready(function() {
-		initSkills();
-	});
-	
-	$(document).on('scroll', function() {
-		initSkills();
-	})
-})
+  function animateValue(obj, start, end, duration) {
+    let startTimestamp = null;
+    const step = (timestamp) => {
+      if (!startTimestamp) startTimestamp = timestamp;
+      const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+      obj.innerHTML = Math.floor(progress * (end - start) + start);
+      if (progress < 1) {
+        window.requestAnimationFrame(step);
+      }
+    };
+    window.requestAnimationFrame(step);
+  }
+}
+
+initSkills();
