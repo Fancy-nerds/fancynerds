dcb37bff64b8891ca235973db3c955b11f0b9945
dmitriiii <dimitriy1988wl@gmail.com>
2021-05-19 14:24:36 +0300

    added google analytics

--- header.php
+++ header.php
@@ -27,6 +27,16 @@
 
 	<?php wp_head(); ?>
 
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-8K04121YRN"></script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+
+	  gtag('config', 'G-8K04121YRN');
+	</script>
+
 	<style>
 		/* The Modal (background) */
 		.modal {
