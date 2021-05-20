07a77d79e6e7526613f74534296f1ef697dd0299
Delave-las-Kure <t1evgeny93@gmail.com>
2021-05-20 19:54:32 +0300

    [b]faq-list: fixed non clickable arrow

--- components/blocks/faq-list/faq-list.php
+++ components/blocks/faq-list/faq-list.php
@@ -22,43 +22,50 @@ extract(M4Helpers::prepBlock($block));
         <div class="faq-list__grid">
             <div class="faq-list__col">
                 <div class="faq-list__item">
-                    <div class="faq-list__title">Best Practices for Keyword Density</div>
+                    <div class="faq-list__title">Best Practices for Keyword Density
+                        <div class="faq-list__arrow">
+                            <img src="<?php bloginfo('template_directory'); ?>/assets/images/faq_arrow.png">
+                        </div>
+                    </div>
                     <div class="faq-list__text">
                         Google has said for years that the most important single factor to them is high quality content.
                         Now more than ever, they have the ability.
-                    </div>
-                    <div class="faq-list__arrow">
-                        <img src="<?php bloginfo('template_directory'); ?>/assets/images/faq_arrow.png">
                     </div>
                 </div>
             </div>
             <div class="faq-list__col">
                 <div class="faq-list__item">
-                    <div class="faq-list__title">Social Media’s Role in SEO</div>
+                    <div class="faq-list__title">Social Media’s Role in SEO
+                        <div class="faq-list__arrow">
+                            <img src="<?php bloginfo('template_directory'); ?>/assets/images/faq_arrow.png">
+                        </div>
+                    </div>
                     <div class="faq-list__text">
                         Google has said for years that the most important single factor to them is high quality content.
                         Now more than ever, they have the ability.
-                    </div>
-                    <div class="faq-list__arrow">
-                        <img src="<?php bloginfo('template_directory'); ?>/assets/images/faq_arrow.png">
                     </div>
                 </div>
             </div>
             <div class="faq-list__col">
                 <div class="faq-list__item">
-                    <div class="faq-list__title">Best SEO Practices for Page Layouts</div>
+                    <div class="faq-list__title">Best SEO Practices for Page Layouts
+                        <div class="faq-list__arrow">
+                            <img src="<?php bloginfo('template_directory'); ?>/assets/images/faq_arrow.png">
+                        </div>
+                    </div>
                     <div class="faq-list__text">
                         Google has said for years that the most important single factor to them is high quality content.
                         Now more than ever, they have the ability. Google has said for years that the most important single factor to
                     </div>
-                    <div class="faq-list__arrow">
-                        <img src="<?php bloginfo('template_directory'); ?>/assets/images/faq_arrow.png">
-                    </div>
                 </div>
             </div>
             <div class="faq-list__col">
                 <div class="faq-list__item">
-                    <div class="faq-list__title">How Does Off Site SEO Work?</div>
+                    <div class="faq-list__title">How Does Off Site SEO Work?
+                        <div class="faq-list__arrow">
+                            <img src="<?php bloginfo('template_directory'); ?>/assets/images/faq_arrow.png">
+                        </div>
+                    </div>
                     <div class="faq-list__text">
                         Google has said for years that the most important single factor to them is high quality content.
                         Now more than ever, they have the ability. Google has said for years that the most important single factor to them is high quality content.
@@ -66,59 +73,60 @@ extract(M4Helpers::prepBlock($block));
                         Now more than ever, they have the ability. Google has said for years that the most important single factor to them is high quality content.
                         Now more than ever, they have the ability.
                     </div>
-                    <div class="faq-list__arrow">
-                        <img src="<?php bloginfo('template_directory'); ?>/assets/images/faq_arrow.png">
-                    </div>
                 </div>
             </div>
             <div class="faq-list__col">
                 <div class="faq-list__item">
-                    <div class="faq-list__title">What is off page SEO link building?</div>
+                    <div class="faq-list__title">What is off page SEO link building?
+                        <div class="faq-list__arrow">
+                            <img src="<?php bloginfo('template_directory'); ?>/assets/images/faq_arrow.png">
+                        </div>
+                    </div>
                     <div class="faq-list__text">
                         Google has said for years that the most important single factor to them is high quality content.
                         Now more than ever, they have the ability.
                     </div>
-                    <div class="faq-list__arrow">
-                        <img src="<?php bloginfo('template_directory'); ?>/assets/images/faq_arrow.png">
-                    </div>
                 </div>
             </div>
             <div class="faq-list__col">
                 <div class="faq-list__item">
-                    <div class="faq-list__title">Why is SEO Link Building Important?</div>
+                    <div class="faq-list__title">Why is SEO Link Building Important?
+                        <div class="faq-list__arrow">
+                            <img src="<?php bloginfo('template_directory'); ?>/assets/images/faq_arrow.png">
+                        </div>
+                    </div>
                     <div class="faq-list__text">
                         Google has said for years that the most important single factor to them is high quality content.
                         Now more than ever, they have the ability. Google has said for years
                     </div>
-                    <div class="faq-list__arrow">
-                        <img src="<?php bloginfo('template_directory'); ?>/assets/images/faq_arrow.png">
-                    </div>
                 </div>
             </div>
             <div class="faq-list__col">
                 <div class="faq-list__item">
-                    <div class="faq-list__title">Best SEO Practices for High Quality Content</div>
+                    <div class="faq-list__title">Best SEO Practices for High Quality Content
+                        <div class="faq-list__arrow">
+                            <img src="<?php bloginfo('template_directory'); ?>/assets/images/faq_arrow.png">
+                        </div>
+                    </div>
                     <div class="faq-list__text">
                         Google has said for years that the most important single factor to them is high quality content.
                         Now more than ever, they have the ability. Google has said for years that the most important single factor to them is high quality content.
                         Now more than ever, they have the ability. Google has said for years that the most important single factor to them is high quality content.
                         Now more than ever, they have the ability.
                     </div>
-                    <div class="faq-list__arrow">
-                        <img src="<?php bloginfo('template_directory'); ?>/assets/images/faq_arrow.png">
-                    </div>
                 </div>
             </div>
             <div class="faq-list__col">
                 <div class="faq-list__item">
-                    <div class="faq-list__title">Why is Researching Keywords Important?</div>
+                    <div class="faq-list__title">Why is Researching Keywords Important?
+                        <div class="faq-list__arrow">
+                            <img src="<?php bloginfo('template_directory'); ?>/assets/images/faq_arrow.png">
+                        </div>
+                    </div>
                     <div class="faq-list__text">
                         Google has said for years that the most important single factor to them is high quality content.
                         Now more than ever, they have the ability. Google has said for years that the most important single factor to them is high quality content.
                         Now more than ever, they have the ability.
-                    </div>
-                    <div class="faq-list__arrow">
-                        <img src="<?php bloginfo('template_directory'); ?>/assets/images/faq_arrow.png">
                     </div>
                 </div>
             </div>
