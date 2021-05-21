d6318e2a46b0a16bf27e6312944b8f51c6c26d4e
Delave-las-Kure <t1evgeny93@gmail.com>
2021-05-21 22:33:53 +0300

    [b]team__photo: added container

--- components/blocks/team__photo/team__photo.php
+++ components/blocks/team__photo/team__photo.php
@@ -16,21 +16,24 @@ $subtitle = get_field('subtitle') ?: 'Your subtitle here...';
 $title = get_field('title') ?: 'Your title here...';
 $description = get_field('description') ?: 'Your description here...';
 /* Render screenshot for preview */
-if (get_field('is_example',$block['id'])) :
-	echo "<img src='".get_template_directory_uri()."/components/blocks/".$block['title']."/".$block['title'].".png'/>";
+if (get_field('is_example', $block['id'])) :
+	echo "<img src='" . get_template_directory_uri() . "/components/blocks/" . $block['title'] . "/" . $block['title'] . ".png'/>";
 	return;
 endif; ?>
-<div <?= $style;?> id="<?php echo esc_attr($id); ?>" class="<?php echo esc_attr($className); ?>">
-	<div class="team__photo-inner">
-	<?php
-	if($image):
-		echo M4Helpers::getImgHtml([ 'img_id'=>$image, 'size'=>'large', 'class'=>'image--rounded ']); 
-	endif;?>
-	<div class="team__heading">
-		<h4 class="subtitle subtitle__dot-before"><?= $subtitle; ?></h4>
-		<h2 class="title"><?= $title; ?></h2>
-		<p class="description"><?= $description; ?></p>
+<div <?= $style; ?> id="<?php echo esc_attr($id); ?>" class="<?php echo esc_attr($className); ?>">
+	<div class="container">
+		<div class="team__photo-inner">
+			<?php
+			if ($image) :
+				echo M4Helpers::getImgHtml(['img_id' => $image, 'size' => 'large', 'class' => 'image--rounded ']);
+			endif; ?>
+			<div class="team__heading">
+				<h4 class="subtitle subtitle__dot-before"><?= $subtitle; ?></h4>
+				<h2 class="title"><?= $title; ?></h2>
+				<p class="description"><?= $description; ?></p>
+			</div>
+		</div>
+
 	</div>
-	</div>
-	
+
 </div>
