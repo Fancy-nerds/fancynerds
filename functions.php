fc6e20c2f15f594386815d4205924fa199abcc52
Delave-las-Kure <t1evgeny93@gmail.com>
2021-05-30 22:45:57 +0300

    added api keys page

--- functions.php
+++ functions.php
@@ -110,6 +110,14 @@ if (!function_exists('fancynerds_setup')) :
 				'flex-height' => true,
 			)
 		);
+
+		if( function_exists('acf_add_options_page') ) {
+			acf_add_options_page(array(
+				'page_title' 	=> 'API Keys',
+				'menu_title'	=> 'API Keys',
+				'menu_slug'	=> 'api-keys-settings',
+			));
+		}
 	}
 endif;
 add_action('after_setup_theme', 'fancynerds_setup');
