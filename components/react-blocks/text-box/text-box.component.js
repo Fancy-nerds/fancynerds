656ee59b1a57c7d1698dadc4e4d372877470f7f5
Delave-las-Kure <t1evgeny93@gmail.com>
2021-06-16 20:57:02 +0300

    [b]text box: add description

--- components/react-blocks/text-box/text-box.component.js
+++ components/react-blocks/text-box/text-box.component.js
@@ -3,12 +3,13 @@
   const useBlockProps = blockEditor.useBlockProps;
   const InnerBlocks = blockEditor.InnerBlocks;
 
-  function TextBoxEdit() {
+  function TextBoxEdit({ isSelected }) {
     const blockProps = useBlockProps({
       className: "text-box",
       style: {
-        border: "1px dashed #494949",
-        padding: '0 15px',
+        border: isSelected ? "1px dashed #494949" : "1px dashed #a6a6a6",
+        padding: "30px",
+        margin: 0,
       },
     });
     return el("div", blockProps, el(InnerBlocks));
@@ -25,6 +26,7 @@
   blocks.registerBlockType("fancy-blocks/text-box", {
     apiVersion: 2,
     title: "Text Box",
+    description: "This block contains styles for text elements such as p, h1-h6, code, link and etc. You can use it to display post content.",
     icon: "media-document",
     category: "fancy-containers",
     edit: TextBoxEdit,
