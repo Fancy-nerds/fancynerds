95c65480292e53987ed257fe484d14c29a1d7c34
dmitriiii <dimitriy1988wl@gmail.com>
2021-06-28 12:39:16 +0300

    update webhook bitrix24

--- libs/crest/settings.php
+++ libs/crest/settings.php
@@ -2,7 +2,7 @@
 //define('C_REST_CLIENT_ID','local.5c8bb1b0891cf2.87252039');//Application ID
 //define('C_REST_CLIENT_SECRET','SakeVG5mbRdcQet45UUrt6q72AMTo7fkwXSO7Y5LYFYNCRsA6f');//Application key
 // or
-define('C_REST_WEB_HOOK_URL','https://fancynerds.bitrix24.de/rest/15/5c1y76cb3vzv72mz/');//url on creat Webhook
+define('C_REST_WEB_HOOK_URL','https://fancynerds.bitrix24.de/rest/15/mzigv7822otgcuow/');//url on creat Webhook
 
 define('C_REST_AFF_ID','UF_CRM_1618697414');//Affiliate ID
 define('C_REST_AFF_EMAIL','UF_CRM_1618700015');//Affiliate Email
