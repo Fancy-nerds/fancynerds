eab913b23efc180a4c3578ec3e55baeba8d86845
Delave-las-Kure <t1evgeny93@gmail.com>
2021-06-29 22:35:54 +0300

    [b]plans: added handler "choose plan"

--- src/scripts/blocks/plans.js
+++ src/scripts/blocks/plans.js
@@ -1,24 +1,33 @@
-// jQuery(function($) {
-//     if ($(window).innerWidth() <= 768) {
-//         $('.plans__slider').slick({
-//             dots: true,
-//             arrows: false,
-//             slidesToShow: 2,
-//             slidesToScroll: 1,
-//             swipeToSlide: 1,
-//             infinite: true,
-//             speed: 500,
-//             swipeToSlide: false,
-//             touchMove: false,
-        
-//             responsive: [
-//                 {
-//                     breakpoint: 568,
-//                     settings: {
-//                         slidesToShow: 1
-//                     }
-//                 }
-//             ]
-//         });
-//     }
-// });
+function initPlansBlock() {
+  document.querySelectorAll(".plans__item").forEach((el) => {
+    const planType = getplanType(el);
+    const modalEl = document.querySelector("#contactUsModal");
+    const contactUsForm = modalEl.querySelector("#global-contact-form");
+    const triggerBtn = el.querySelector(".button");
+
+    initModal(modalEl, triggerBtn, {
+      onOpen() {
+        setFormPlanType(planType, contactUsForm);
+      },
+      onClose() {
+        resetFormPlanType(planType, contactUsForm);
+      },
+    });
+  });
+
+  function getplanType(el) {
+    if (el.classList.contains(".plans__item--standard")) return "standart";
+    if (el.classList.contains(".plans__item--economy")) return "economy";
+    if (el.classList.contains(".plans__item--executive")) return "executive";
+  }
+
+  function setFormPlanType(planType, contactUsForm) {
+    console.log('setFormPlanType')
+  }
+
+  function resetFormPlanType(planType, contactUsForm) {
+    console.log('resetFormPlanType')
+  }
+}
+
+initPlansBlock();
