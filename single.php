2be03f314ed19bb7e8a5ac3a121eb42023eee546
Delave-las-Kure <t1evgeny93@gmail.com>
2021-07-07 23:46:24 +0300

    reimport styles

--- single.php
+++ single.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying all single posts
  *
@@ -9,31 +10,290 @@
 
 get_header();
 ?>
+<main class="main">
 
-	<main id="primary" class="site-main">
+	<article class="article">
+		<div class="article__hero" style="background-image: url('../../assets/images/article-image.png');">
+			<div class="container">
+				<div class="article__heading">
+					<div class="article-label">Marketing</div>
+					<h1 class="title">15 SEO Best Practices: Website Architecture</h1>
+					<div class="article-state">
+						<div class="article-state__item article-state__author">
+							<i class="flaticon-user"></i>
+							By Pablo Villalpando
+						</div>
+						<div class="article-state__item article-state__date">
+							<i class="flaticon-clock"></i>
+							October 10, 2019
+						</div>
+						<div class="article-state__item article-state__comments">
+							<i class="flaticon-chat"></i>
+							0 Comments
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+
+		<div class="container">
+			<div class="article__content">
+				<div class="article__main">
+					<p>
+						One of the principal decisions that startup owners have to make is whether or not to engage in SEO.
+						One common consideration is the cost of investing in an SEO campaign versus possible returns.
+						Another is the daunting list of SEO terminologies, which may overwhelm newcomers to the e-commerce
+						and website scene. If you are a new online entrepreneur but are not convinced of the benefits of
+						SEO, then this article is for you! Here are some of the reasons why SEO is important and why
+						you should consider it as a marketing technique. <b>SEO is less expensive than other online marketing
+							approaches.</b> It also offers higher reward rates when compared to other techniques such as social
+						media marketing, pay per click advertising, and email marketing. While you may need to defray a
+						substantial amount to cover initial SEO planning and processes that include website design,
+						programming, and strategizing, you can be sure to get faster and more long-term results.
+					</p>
+					<div class="blockquote">
+						<div class="blockquote__text">“I cannot give you the formula for success, but I can give you the formula for failure. It is: Try to please everybody.”</div>
+						<div class="blockquote__author">Herbert Bayard Swope</div>
+					</div>
+					<p>
+						Implementing SEO strategies help you to rank higher on the search engine’s results page (SERP).
+						This means that when your target customers search for products and services that your industry offers,
+						they are <a href="#">likely to find your website</a>. When you repeatedly appear on the SERP, users become aware of
+						your site and your business. This increases the chances of landing potential customers on your webpages.
+					</p>
+					<div class="gallery">
+						<div class="row">
+							<div class="col col-33">
+								<img src="../../assets/images/1975.png">
+							</div>
+							<div class="col col-33">
+								<img src="../../assets/images/1824.png">
+							</div>
+							<div class="col col-33">
+								<img src="../../assets/images/1816.png">
+							</div>
+						</div>
+					</div>
+					<h2>SEO is a Cost-Effective Advertising Strategy</h2>
+					<p>
+						One SEO component called off-site SEO ensures that users of external pages or of social media can find your website.
+						Being optimized for SEO increases the site’s potential to draw customers from other platforms other than the search engine.
+						]For instance, when you post content and links to your page on Facebook or Twitter, you are able to promote your website
+						and attract more customers. One of the main goals of SEO is to attract the targeted audience <a href="#">through organic searches</a>.
+						The number of visitors to your site influences your sales and subscriptions. It also promotes the marketing of your
+						products and services. Increased inbound traffic is always good for business because it equates to more conversion opportunities.
+					</p>
+
+					<p>
+						Some SEO-related tools such as the Google Keyword Planner and Google Analytics provide quantitative data to help
+						you understand your market, analyze the trends, and know your competitors’ standings. It helps you identify popular
+						and valuable keywords so you can decide how to <b>structure or revise your content</b>. It also gives you insights on your
+						market’s behavior such as location, times of activity, frequency of searches, technologies used, product preferences,
+						etc. All these metrics are useful in helping you get to know your audience, their needs, and their expectations.
+					</p>
+
+					<div class="article__footer">
+						<div class="article__actions">
+							<div class="article__tags">
+
+							</div>
+							<div class="article__socials">
+
+							</div>
+						</div>
+
+						<div class="author">
+							<div class="author__image">
+								<img src="../../assets/images/15113.png">
+							</div>
+							<div class="author__content">
+								<div class="author__work">Author</div>
+								<div class="author__name">Pablo Vilalpando</div>
+								<div class="author__text">He is attended the State University of New York at Oswego where he majored in English Literature and Creative Writing.</div>
+								<div class="author__social">
+									<a href="#" class="author__social--icon author__social--tw">
+										<svg xmlns="http://www.w3.org/2000/svg" width="13" height="11">
+											<path fill="#a5b7d2" d="M12.883 1.698a5.475 5.475 0 01-1.518.393A2.537 2.537 0 0012.527.704a5.446 5.446 0 01-1.679.609 2.709 2.709 0 00-1.93-.79c-1.459 0-2.642 1.12-2.642 2.504 0 .197.023.388.068.57C4.148 3.496 2.2 2.498.896.98a2.4 2.4 0 00-.358 1.26c0 .87.467 1.636 1.176 2.087a2.762 2.762 0 01-1.197-.315v.032c0 1.213.91 2.227 2.12 2.456a2.815 2.815 0 01-1.194.044c.337.995 1.313 1.719 2.47 1.739A5.479 5.479 0 01.63 9.356c-.213 0-.424-.012-.63-.036a7.776 7.776 0 004.051 1.128c4.862 0 7.52-3.82 7.52-7.13 0-.11-.002-.216-.007-.324a5.236 5.236 0 001.319-1.296z" />
+										</svg>
+									</a>
+									<a href="#" class="author__social--icon author__social--fb">
+										<svg xmlns="http://www.w3.org/2000/svg" width="7" height="13">
+											<path fill="#a5b7d2" d="M2.338 12.972V7.65H.672V5.255h1.704v-1.98S2.453.778 4.642.76h2.433v2.35H5.566s-.638 0-.638.702v1.486h2.185L6.85 7.69H4.966v5.281z" />
+										</svg>
+									</a>
+									<a href="#" class="author__social--icon author__social--in">
+										<svg xmlns="http://www.w3.org/2000/svg" width="14" height="13">
+											<path fill="#a5b7d2" d="M11.178 12.205v-4.63s-.148-1.24-1.411-1.24c-1.263 0-1.495 1.179-1.495 1.179v4.691H5.668l.042-7.65h2.52l-.021.964s.526-1.26 2.505-1.26c1.98 0 2.873 1.08 3.042 3.096v4.85zM2.474 3.47c-.896 0-1.622-.607-1.622-1.357S1.578.754 2.474.754c.895 0 1.621.608 1.621 1.358 0 .75-.726 1.357-1.621 1.357zm1.608 8.744H.879V4.575l3.203-.02z" />
+										</svg>
+									</a>
+									<a href="#" class="author__social--icon author__social--insta">
+										<svg xmlns="http://www.w3.org/2000/svg" width="14" height="13">
+											<path fill="#a5b7d2" d="M4.878.018h2.984c.481 0 1.088-.06 1.52.025h.65l1.004.128c.924.246 1.684.68 2.17 1.336.752 1.015.76 2.588.76 4.342v1.85c0 .297.03.671-.027.924v.616c-.08.349-.054.73-.163 1.054-.313.933-.828 1.64-1.628 2.107-1.024.597-2.68.563-4.34.564H5.855c-.314 0-.71.03-.977-.025h-.651c-.368-.075-.77-.051-1.112-.154-1.136-.342-2-.961-2.442-1.953-.212-.476-.22-1.01-.352-1.593v-.616C.264 8.37.294 7.996.294 7.698v-1.8c0-1.18-.074-2.472.244-3.389C.962 1.286 1.876.527 3.223.171L4.227.043c.196-.04.503.062.65-.025zM4.797 1.2c-.146.083-.988.044-1.221.102-.866.217-1.475.654-1.79 1.388-.307.713-.245 1.8-.245 2.773 0 2.23-.342 4.901 1.058 5.831 1.062.706 3.362.488 5.127.488 1.34 0 2.926.11 3.771-.385.367-.215.745-.585.922-.975.487-1.067.299-3.392.299-4.907 0-1.371.012-2.64-.597-3.416-.395-.504-1.023-.687-1.79-.848l-.868-.026C9.015 1.134 8.388 1.2 7.889 1.2H4.797zm5.832 1.051c1.194-.03 1.267 1.34.299 1.568-.574.137-1.155-.501-.95-1.027.135-.346.362-.353.651-.54zm-3.635.9c1.725-.015 2.818.862 3.337 2.005.32.706.428 1.781.081 2.57-.424.955-1.218 1.644-2.306 1.976-.26.079-.56.073-.84.129-.473.094-1.26-.19-1.547-.309-1.396-.572-2.716-2.35-1.872-4.263.378-.857 1.076-1.518 1.98-1.877.257-.1.561-.144.841-.205.113-.025.249.02.326-.025zm-.027 1.182c-.158.092-.527.082-.705.155a2.341 2.341 0 00-1.303 1.31c-.47 1.226.394 2.23 1.194 2.62.244.119.716.32 1.14.232.977-.205 1.6-.557 1.953-1.336.71-1.571-.682-2.992-2.28-2.98z" />
+										</svg>
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<div class="related-posts">
+						<h2 class="title">Related Posts</h2>
+						<div class="row">
+							<div class="col col-50">
+								<div class="card">
+									<div class="article-label">Marketing</div>
+									<div class="card__image">
+										<img src="assets/images/31147.png">
+									</div>
+									<div class="card__content">
+										<div class="article-state">
+											<div class="article-state__item article-state__author">
+												<i class="flaticon-user"></i>
+												By Pablo Villalpando
+											</div>
+											<div class="article-state__item article-state__date">
+												<i class="flaticon-clock"></i>
+												October 10, 2019
+											</div>
+										</div>
+										<div class="card__title">What Makes a Quality Backlink?</div>
+										<div class="card__excerpt paragraph">Our campaigns get your business in front of the right people at the right time...</div>
+									</div>
+								</div>
+							</div>
+							<div class="col col-50">
+								<div class="card">
+									<div class="article-label">Marketing</div>
+									<div class="card__image">
+										<img src="assets/images/31147.png">
+									</div>
+									<div class="card__content">
+										<div class="article-state">
+											<div class="article-state__item article-state__author">
+												<i class="flaticon-user"></i>
+												By Pablo Villalpando
+											</div>
+											<div class="article-state__item article-state__date">
+												<i class="flaticon-clock"></i>
+												October 10, 2019
+											</div>
+										</div>
+										<div class="card__title">What Makes a Quality Backlink?</div>
+										<div class="card__excerpt paragraph">Our campaigns get your business in front of the right people at the right time...</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<div class="comments">
+						<h2 class="title">
+							Comments
+							<span class="comments__count">(02)</span>
+						</h2>
+
+						<div class="comments__wrapper">
+							<div class="comment">
+								<div class="comment__image">
+									<img src="../../assets/images/author-image.png">
+								</div>
+								<div class="comment__content">
+									<div class="comment__name">Liza Benedict</div>
+									<div class="comment__date">2 days ago</div>
+									<div class="comment__text">
+										SEO is always changing so leaving the strategy and tactics to Onum has more than paid for itself.
+										We estimate ROI is over 10 to 1 – I can’t say enough about this team.
+									</div>
+								</div>
+
+								<a href="#" class="comment__link">Reply</a>
+							</div>
+
+							<div class="comment comment__reply">
+								<div class="comment__image">
+									<img src="../../assets/images/author-image.png">
+								</div>
+								<div class="comment__content">
+									<div class="comment__name">Liza Benedict</div>
+									<div class="comment__date">2 days ago</div>
+									<div class="comment__text">
+										SEO is always changing so leaving the strategy and tactics to Onum has more than paid for itself.
+										We estimate ROI is over 10 to 1 – I can’t say enough about this team.
+									</div>
+								</div>
+
+								<a href="#" class="comment__link">Reply</a>
+							</div>
+
+						</div>
+					</div>
+
+					<form class="form comments__form">
+						<h2 class="title">Leave a Comment</h2>
+						<div class="form__group">
+							<div class="row">
+								<div class="col col-50">
+									<input placeholder="Your Name *" type="text" class="form__control" />
+								</div>
+								<div class="col col-50">
+									<input placeholder="Your Email *" type="text" class="form__control" />
+								</div>
+							</div>
+						</div>
+						<div class="form__group">
+							<input placeholder="Website" type="text" class="form__control" />
+						</div>
+						<div class="form__group">
+							<textarea placeholder="Message..." type="text" class="form__control"></textarea>
+						</div>
+						<div class="form__buttons">
+
+						</div>
+					</form>
+				</div>
+
+				<div class="article__sidebar">
+					<div class="ceo">
+						<div class="ceo__image">
+							<img src="../../assets/images/author-image.png">
+						</div>
+						<div class="ceo__name">Kate Olson</div>
+						<div class="ceo__description">She is the CEO. She's a big fan her cat Tux, & dinner parties.</div>
+						<div class="ceo__social"></div>
+					</div>
+				</div>
+			</div>
+
+		</div>
+	</article>
+
+</main>
+<!--<main id="primary" class="site-main">
 
 		<?php
-		while ( have_posts() ) :
+		while (have_posts()) :
 			the_post();
 
-			get_template_part( 'template-parts/content', get_post_type() );
+			get_template_part('template-parts/content', get_post_type());
 
 			the_post_navigation(
 				array(
-					'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'fancynerds' ) . '</span> <span class="nav-title">%title</span>',
-					'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'fancynerds' ) . '</span> <span class="nav-title">%title</span>',
+					'prev_text' => '<span class="nav-subtitle">' . esc_html__('Previous:', 'fancynerds') . '</span> <span class="nav-title">%title</span>',
+					'next_text' => '<span class="nav-subtitle">' . esc_html__('Next:', 'fancynerds') . '</span> <span class="nav-title">%title</span>',
 				)
 			);
 
 			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
+			if (comments_open() || get_comments_number()) :
 				comments_template();
 			endif;
 
 		endwhile; // End of the loop.
 		?>
 
-	</main><!-- #main -->
+	</main>-->
+<!-- #main -->
 
 <?php
 get_sidebar();
