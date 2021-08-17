2e5714fea241321100d3a2e6019bd4364018331b
Delave-las-Kure <t1evgeny93@gmail.com>
2021-08-17 20:21:08 +0300

    added empty msg

--- home.php
+++ home.php
@@ -1,28 +1,40 @@
-<?
+<?php
+
 /**
- * Blog
+ * The template for displaying archive pages
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package fancynerds
  */
+
 get_header(); ?>
 
 <div class="archive">
 	<div class="archive__banner">
 		<? get_template_part('template-parts/jumbotron__inner', null, [
-			'title' => __('Blog', 'fancynerds'),
+			'title' => get_the_archive_title(),
 		]); ?>
 	</div>
 	<div class="container">
 		<div class="archive__content">
-			<div class="archive__grid">
-				<? if (have_posts()) :
-					while (have_posts()) : the_post(); ?>
+
+			<? if (have_posts()) { ?>
+				<div class="archive__grid">
+					<? while (have_posts()) : the_post(); ?>
 						<?= get_template_part('template-parts/post', 'card', [
 							'post_id' => $post->ID
 						]); ?>
-				<?
-					endwhile;
-				endif;
-				?>
-			</div>
+					<?
+					endwhile; ?>
+				</div>
+			<? } else {
+			?>
+				<h2 class="archive__empty-title"><?= __('Sorry, no posts found', 'fancynerds') ?></h2>
+			<?
+			};
+			?>
+
 			<div class="archive__sidebar">
 				<?
 				get_sidebar('archive');
