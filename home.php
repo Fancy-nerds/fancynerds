dbd776b36b51fbe63035a176e7b89442fee72139
Delave-las-Kure <t1evgeny93@gmail.com>
2021-08-23 23:01:29 +0300

    added pagination

--- home.php
+++ home.php
@@ -20,13 +20,25 @@ get_header(); ?>
 		<div class="archive__content">
 
 			<? if (have_posts()) { ?>
-				<div class="archive__grid">
-					<? while (have_posts()) : the_post(); ?>
-						<?= get_template_part('template-parts/post', 'card', [
-							'post_id' => $post->ID
-						]); ?>
-					<?
-					endwhile; ?>
+				<div class="archive__wrapper">
+					<div class="archive__grid">
+						<? while (have_posts()) : the_post(); ?>
+							<?= get_template_part('template-parts/post', 'card', [
+								'post_id' => $post->ID
+							]); ?>
+						<?
+						endwhile; ?>
+					</div>
+					<? if ($pag = get_the_posts_pagination([
+						'mid_size'  => 2,
+						'class' => 'pagination',
+						'prev_text' => '<i class="flaticon-arrow-pointing-to-left"></i>',
+						'next_text' => '<i class="flaticon-arrow-pointing-to-right"></i>'
+					])) { ?>
+						<div class="archive__pagination">
+							<?= $pag ?>
+						</div>
+					<? } ?>
 				</div>
 			<? } else {
 			?>
