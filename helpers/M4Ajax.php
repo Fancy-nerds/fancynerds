0614b2d91ee2b22a8e077bb5e185d22f628e78d3
Delave-las-Kure <t1evgeny93@gmail.com>
2021-10-20 21:36:05 +0300

    fixed broken web hook url

--- helpers/M4Ajax.php
+++ helpers/M4Ajax.php
@@ -1,5 +1,5 @@
 <?php
-require_once ( get_template_directory() . '/libs/crest/crest.php' );
+require_once(get_template_directory() . '/libs/crest/crest.php');
 
 class M4Ajax
 {
@@ -10,60 +10,67 @@ class M4Ajax
 		$this->wpdb = $wpdb;
 
 		#Form with leads
-		add_action('wp_ajax_nopriv_m4ajx_lead', array( $this, 'lead' ));
-		add_action('wp_ajax_m4ajx_lead', array( $this, 'lead' ));
-
+		add_action('wp_ajax_nopriv_m4ajx_lead', array($this, 'lead'));
+		add_action('wp_ajax_m4ajx_lead', array($this, 'lead'));
 	}
-	public function final( $arrr ){
-		echo json_encode( $arrr );
+	public function final($arrr)
+	{
+		echo json_encode($arrr);
 		exit();
 	}
 
 
 	#Load
-	public function lead(){
+	public function lead()
+	{
 		$arrr = array();
-		$fields=['ASSIGNED_BY_ID'=>C_REST_ASSIGNED_BY_ID];
-		
-		extract( $_POST );
-		
-		foreach ($dataSubm as $k=>$v) :
-			if( $v['name']=='EMAIL'||$v['name']=='PHONE' ){
-				$fields[$v['name']]=[["VALUE"=>$v['value'],"VALUE_TYPE" => "WORK"]];
+		$fields = ['ASSIGNED_BY_ID' => C_REST_ASSIGNED_BY_ID];
+
+		extract($_POST);
+
+		foreach ($dataSubm as $k => $v) :
+			if ($v['name'] == 'EMAIL' || $v['name'] == 'PHONE') {
+				$fields[$v['name']] = [["VALUE" => $v['value'], "VALUE_TYPE" => "WORK"]];
 				continue;
 			}
-			$fields[$v['name']]=$v['value'];
+			$fields[$v['name']] = $v['value'];
 		endforeach;
 
 		$result = M4Helpers::checkRecaptchaFilter($fields['g-recaptcha-response']);
 
 		if (!$result['success']) {
-			wp_send_json_error( array(
-				'message' => 'Invalid CAPTCHA token. Please try again.'
+			wp_send_json_error(array(
+				'message' => __('Invalid CAPTCHA token. Please try again.', 'fancynerds')
 			));
 			return;
 		}
 
 		#get First name / Last name / EMAIL / of affiliate
-		if( isset($fields[C_REST_AFF_ID]) && $fields[C_REST_AFF_ID] !== '' ){
+		if (isset($fields[C_REST_AFF_ID]) && $fields[C_REST_AFF_ID] !== '') {
 			$aff_users = $this->wpdb->prefix . "aff_affiliates";
-			$sql = $this->wpdb->prepare( "SELECT * FROM {$aff_users} WHERE affiliate_id=%d", $fields[C_REST_AFF_ID] );
-			$aff_user = $this->wpdb->get_results( $sql,ARRAY_A );
-			if(is_array($aff_user) && count($aff_user)>0){
+			$sql = $this->wpdb->prepare("SELECT * FROM {$aff_users} WHERE affiliate_id=%d", $fields[C_REST_AFF_ID]);
+			$aff_user = $this->wpdb->get_results($sql, ARRAY_A);
+			if (is_array($aff_user) && count($aff_user) > 0) {
 				$fields[C_REST_AFF_NAME] = $aff_user[0]['name'];
 				$fields[C_REST_AFF_EMAIL] = $aff_user[0]['email'];
 			}
 		}
-		CRest::call(
+		$crest_res = CRest::call(
 			'crm.lead.add',
 			[
-		    'fields' =>$fields
+				'fields' => $fields
 			]
 		);
+
 		$arrr['success'] = true;
- 		wp_send_json_success(array(
-			 'message' => 'We received your message, and we will contact you soon!'
-		 ));
+		if ($crest_res['error'])
+			wp_send_json_error(array(
+				'message' => __('Server error. Please try again later.', 'fancynerds')
+			));
+		else
+			wp_send_json_success(array(
+				'message' => __('We received your message, and we will contact you soon!', 'fancynerds')
+			));
 	}
 }
 new M4Ajax;
